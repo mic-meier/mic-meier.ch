aaa9a3207edf80a7769d35deb094553d60cc5359
ElMayo <mic.l.meier@gmail.com>
2020-09-23 19:38:01 +0200

    line highlighting and title in code blocks

--- theme.js
+++ theme.js
@@ -1,9 +1,42 @@
 import { roboto } from '@theme-ui/presets'
+import nightOwl from 'prism-react-renderer/themes/nightOwl'
+
+console.log('nightOwl', nightOwl)
 
 const theme = {
   ...roboto,
   styles: {
     ...roboto.styles,
+    CodeBlock: {
+      overflow: `auto`,
+      pre: {
+        backgroundColor: `transparent`,
+        float: `left`,
+        minWidth: `100%`,
+        margin: 0,
+        padding: '1rem',
+      },
+      highlightLine: {
+        backgroundColor: `#45474e`,
+        borderLeftColor: `#49d0c5`,
+        borderLeftStyle: `solid`,
+        borderLeftWidth: `0.25em`,
+        display: `block`,
+        marginRight: `-1em`,
+        marginLeft: `-1em`,
+        paddingRight: `1em`,
+        paddingLeft: `0.75em`,
+      },
+      title: {
+        fontFamily: `mono`,
+        backgroundColor: nightOwl.plain.backgroundColor,
+        borderBottomWidth: `2px`,
+        borderBottomStyle: `solid`,
+        borderBottomColor: `#f0f0f0`,
+        color: nightOwl.plain.color,
+        padding: '0.5rem 1rem',
+      },
+    },
   },
   links: {
     nav: {
@@ -13,6 +46,5 @@ const theme = {
     },
   },
 }
-console.log('theme', theme)
 
 export default theme
